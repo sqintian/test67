954d76f5324aa6e279be4f3f04795bda00f05cc5
LGH <LGH@sumac.local>
2017-02-09 16:38:57 +0800

    初始化项目

---
+++ 67/67/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  67
+//
+//  Created by LGH on 17/2/9.
+//  Copyright © 2017年 LGH. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ 67/67/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  67
+//
+//  Created by LGH on 17/2/9.
+//  Copyright © 2017年 LGH. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
